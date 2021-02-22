1d803b695512b663782365d6e3420a479c46712c
yeonju-oho <yeonju-oho@users.noreply.github.com>
2021-02-22 23:46:59 +0900

    2.1 Distance Converter 코딩

---
+++ PracticalCS/DistanceConverter/FeetConverter.cs
@@ -0,0 +1,18 @@
+﻿namespace DistanceConverter
+{
+    public static class FeetConverter
+    {
+        private const double RATIO = 0.3048;    //공개할 필요가 있을 경우 public static readonly를 사용한다.
+
+        public static double ToMeter(double feet)
+        {
+            if (feet == 0) return 0;
+            else return feet * RATIO;
+        }
+        public static double FromMeter(double meter)
+        {
+            if (meter == 0) return 0;
+            else return meter / RATIO;
+        }
+    }
+}
---
+++ PracticalCS/DistanceConverter/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+/// <summary>
+/// 2.1 Distance Converter에 관한 소스코드이다.
+/// </summary>
+namespace DistanceConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string readLine = Console.ReadLine();
+
+            if (readLine.Equals("-tom"))
+            {
+                PrintFeetToMeter(1, 100);
+            }
+            else if (readLine.Equals("-tof"))
+            {
+                PrintMeeterToFeet(1, 100);
+            }
+            else
+            {
+                Console.WriteLine("일치하는 문자열이 없습니다!!");
+            }
+
+            Console.ReadKey();
+        }
+
+        public static void PrintFeetToMeter(int start, int stop)
+        {
+            for (int i = start; i <= stop; i++)
+            {
+                Console.WriteLine("{0} ft = {1:0.0000} m", i, FeetConverter.ToMeter(i));
+            }
+        }
+        public static void PrintMeeterToFeet(int start, int stop)
+        {
+            for (int i = start; i <= stop; i++)
+            {
+                Console.WriteLine("{0} m = {1:0.0000} ft", i, FeetConverter.FromMeter(i));
+            }
+        }
+    }
+}
